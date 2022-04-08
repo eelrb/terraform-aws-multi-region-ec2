eab51592e14baed714f2c1064c54a32d9bb04796
Brian Lee <35783300+eelrb@users.noreply.github.com>
2022-04-07 22:33:02 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,9 +8,11 @@ provider "aws" {
 }
 
 
-data "aws_ami" "my_image" {
+data "aws_ami" "my_image_1" {
   
   most_recent=true
+  
+  provider = aws
 
   filter {
       name   = "name"
@@ -25,6 +27,27 @@ data "aws_ami" "my_image" {
     owners = ["099720109477"] # Canonical
 
 }
+
+data "aws_ami" "my_image_2" {
+  
+  most_recent=true
+  
+  provider = aws.us-east-2
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+    }
+
+    filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+    }
+
+    owners = ["099720109477"] # Canonical
+
+}
+
 
 resource "aws_instance" "blee-ec2" {
   count = "${var.instance_count}"
@@ -43,7 +66,7 @@ resource "aws_instance" "blee-ec2" {
 
 resource "aws_instance" "blee-ec2-2" {
   count = "${var.instance_count}"
-  ami           = "${data.aws_ami.my_image.id}"
+  ami           = "${data.aws_ami.my_image_2.id}"
   instance_type = "t2.micro"
   #vpc_security_group_ids = ["${aws_security_group.main_sec_group.id}"]
   #subnet_id = "${aws_subnet.subnet1.id}"

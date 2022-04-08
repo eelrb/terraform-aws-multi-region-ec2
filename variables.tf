84b6287c63e5e95b2f91a7a6b2857eb3832bbc98
Brian Lee <35783300+eelrb@users.noreply.github.com>
2022-04-07 23:37:00 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,14 +2,12 @@ variable "instance_count" {
 default = 1
 }
 
-variable "key_name" {
-  default = ""
-}
-
 variable "aws_region_1" {
 default = "us-west-1"
   }
-  
+
+/*  
 variable "aws_region_2" {
 default = "us-west-2"
   }
+*/
